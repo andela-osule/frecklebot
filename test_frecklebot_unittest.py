446a972cd0cd5130a679d1ceaf7f45ab1772fba3
Oluwafemi Sule <thezurg07@gmail.com>
2015-07-11 22:26:49 +0100

    Test subscribe view function

--- test_frecklebot_unittest.py
+++ test_frecklebot_unittest.py
@@ -5,13 +5,17 @@ from flask import json
 class TestFrecklebot(unittest.TestCase):
     """Test Frecklebot """
     def setUp(self):
+        frecklebot.app.testing = True
+        self.config = frecklebot.app.config
         self.app = frecklebot.app.test_client()
         pass
 
     def test_subscribe(self):
-        resp = self.app.get('/')
+        data = {'token':self.config['BOT_APP_OUTGOING_PAYLOAD_TOKEN'],\
+        'user_id':'U07449TRN', 'user_name':'oluwafemi'}
+        resp = self.app.post('incoming', data=data)
         data = json.loads(resp.data)
         #assertEq 'Hello World' in res
-        self.assertEqual(data['message'], "Hello World")
+        self.assertEqual(data['Status'], "OK")
 if __name__ == '__main__':
     unittest.main()
