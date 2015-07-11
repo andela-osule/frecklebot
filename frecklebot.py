1b95da5785421c9af49d541a3a67e0da5fb91103
Oluwafemi Sule <thezurg07@gmail.com>
2015-07-11 13:57:52 +0100

    frecklebot.py

---
+++ frecklebot.py
@@ -0,0 +1,9 @@
+from flask import Flask, json, jsonify 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify(message="Hello World")
+
+if __name__=='__main__':
+    app.run(debug=True) 
